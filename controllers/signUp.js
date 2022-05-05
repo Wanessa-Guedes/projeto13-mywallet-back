b4b067ad7a8402dc3820a1beb66e03fe812a6610
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-04 23:49:41 -0300

    feat: Sign-UP back-end

---
+++ controllers/signUp.js
@@ -0,0 +1,50 @@
+import db from "../db.js";
+import chalk from "chalk";
+import bcrypt from "bcrypt";
+import Joi from "joi";
+
+// Tela de cadastro --> Recebe - name, email, password, confirm
+
+export async function postSignUp (req, res){
+    
+    //const {name, email, password, confirm} = req.body;
+
+    // Fazer verificação com joi
+    const signUpSchema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+        confirm: Joi.ref('password')
+    });
+
+    const {error, value} = signUpSchema.validate(req.body, {abortEarly: false});
+    if(error){
+        res.status(422).send(error.details.map(detail => detail.message));
+        return;
+    };
+
+    const passowrdHash = bcrypt.hashSync(value.password, 10);
+    
+    try {
+        const collection = db.collection('users');
+        const infoUser = {
+            name: value.name,
+            email: value.email,
+            password: passowrdHash
+            };
+        
+        const emailExists = await collection.findOne({email: infoUser.email});
+        if(emailExists){
+            return res.status(404).send(console.log(chalk.bold.green("E-mail já cadastrado.")))
+        }
+        
+        await collection.insertOne(infoUser)
+        console.log(infoUser)
+        //TODO: Tela 1 -- Colocar no banco de dados
+
+        res.status(201).send(console.log(chalk.bold.green("Cadastro realizado com sucesso")));
+
+    } catch (e) {
+        res.status(500).send(console.log(chalk.bold.red("Erro ao logar"), e));
+    }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, {json} from "express";
 import chalk from "chalk";
 import cors from "cors";
 import Joi from "joi";
+import { postSignUp } from "./controllers/signUp.js";
 
 const app = express();
 app.use(json());
@@ -10,7 +11,7 @@ app.use(cors());
 // Iniciando salvando tudo em variáveis (Para poder começar o front)
 // TODO: Retirar variáveis globais e salvar no mongo
 const loginUsersData = [];
-const infosUser = [];
+
 
 //Projeto13 - MyWallet 
 //TODO: Tela 1: Sign-in (post - usuário enviar os dados para entrar no app {e-mail, senha})
@@ -48,41 +49,7 @@ app.post("/sign-in", (req,res) =>{
 })
 
 //TODO: Tela 2: Sign-up (post - usuário cadastra infos {nome, e-mail, senha, confirme a senha})
-app.post("/sign-up", (req,res) =>{
-    
-    const {name, email, password, confirmation} = req.body;
-
-    // Fazer verificação com joi
-    const signUpSchema = Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().required(),
-        password: Joi.string().required(),
-        confirmation: Joi.ref('password')
-    });
-
-    const {error, value} = signUpSchema.validate(req.body, {abortEarly: false});
-    if(error){
-        res.status(422).send(error.details.map(detail => detail.message));
-        return;
-    };
-
-    const infoUser = {
-        name: value.name,
-        email: value.email,
-        password: value.password,
-        confirmation: value.confirmation
-        };
-
-        infosUser.push(infoUser);
-
-        //TODO: Tela 1 -- Colocar no banco de dados
-
-        res.status(201).send(console.log(chalk.bold.green("Cadastro realizado com sucesso")))
-
-        /*  } catch (e) {
-        res.status(500).send(console.log(chalk.bold.red("Erro ao logar"), e));
-    } */
-})
+app.post("/signUp", postSignUp)
 
 //TODO: Tela 3 e tela 6: Tela de registros (get - colocar os registros, 
 //TODO: Tela 4: Opção de nova entrada (post - {valor e descrição})
@@ -91,7 +58,7 @@ app.post("/sign-up", (req,res) =>{
 
 
 // subindo back-end
-const PORTA = 5000;
+const PORTA = process.env.PORTA || 5000;
 app.listen(PORTA, () => {
     console.log(chalk.bold.green(`Back-end on na porta ${PORTA}`))
 });
